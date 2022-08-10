68bd9b523d4a171bddfb84d9ab4cc9215f192e42
max brewer <58602708+deepfates@users.noreply.github.com>
2022-08-10 12:19:52 -0600

    change npm repo

--- components/sparkl-theme/index.ts
+++ components/sparkl-theme/index.ts
@@ -4,13 +4,50 @@ import {
   GeistUIThemesExpressiveness,
   GeistUIThemesLayout,
   GeistUIThemesFont,
+  GeistUIThemesBreakpoints,
 } from 'components/themes'
-import { defaultLayout } from 'components/themes/presets/shared'
 import { Themes } from '..'
 import * as c from './radix-colors'
 
-const layout: GeistUIThemesLayout = defaultLayout
-layout.radius = '2px'
+
+export const breakpoints: GeistUIThemesBreakpoints = {
+  xs: {
+    min: '0',
+    max: '650px',
+  },
+  sm: {
+    min: '650px',
+    max: '900px',
+  },
+  md: {
+    min: '900px',
+    max: '1280px',
+  },
+  lg: {
+    min: '1280px',
+    max: '1920px',
+  },
+  xl: {
+    min: '1920px',
+    max: '10000px',
+  },
+}
+
+export const layout: GeistUIThemesLayout = {
+  gap: '16pt',
+  gapNegative: '-16pt',
+  gapHalf: '8pt',
+  gapHalfNegative: '-8pt',
+  gapQuarter: '4pt',
+  gapQuarterNegative: '-4pt',
+  pageMargin: '16pt',
+  pageWidth: '750pt',
+  pageWidthWithMargin: '782pt',
+  breakpointMobile: breakpoints.xs.max,
+  breakpointTablet: breakpoints.sm.max,
+  radius: '2px',
+  unit: '16px',
+}
 
 export const expressiveness: GeistUIThemesExpressiveness = {
   linkStyle: 'none',
